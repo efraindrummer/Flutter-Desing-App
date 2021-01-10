ac45f2f30643414b9f6bd98f4be7f1b33d41317b
Efrain May <efrain_may_75@hotmail.com>
2021-01-10 13:02:13 -0600

    avance del diseño

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -2,12 +2,38 @@ import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
 
+  final estitloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estitloSubtitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Basico Page'),
-      ),
+      body: Column(
+        children: <Widget>[
+          Image(
+            image: NetworkImage('https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067__340.png'),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Las montañas en un atardecer', style: estitloTitulo,),
+                      SizedBox(height: 7.0,),
+                      Text('Esto es un hermoso', style: estitloSubtitulo,),
+                    ],
+                  ),
+                ),
+                Icon(Icons.star, color: Colors.red, size: 30.0),
+                Text('41', style: TextStyle(fontSize: 20.0))
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
